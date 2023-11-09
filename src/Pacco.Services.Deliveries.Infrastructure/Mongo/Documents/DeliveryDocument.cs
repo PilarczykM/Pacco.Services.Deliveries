5e867d46c7e705bf2d5ce547f7636a5af18e7797
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-11-09 06:58:37 +0100

    Upgrade infra to dotnet 7.0
    Upgrade nuget packages
    Update codestyle

--- src/Pacco.Services.Deliveries.Infrastructure/Mongo/Documents/DeliveryDocument.cs
+++ src/Pacco.Services.Deliveries.Infrastructure/Mongo/Documents/DeliveryDocument.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
+
 using Convey.Types;
+
 using Pacco.Services.Deliveries.Core.Entities;
 
-namespace Pacco.Services.Deliveries.Infrastructure.Mongo.Documents
+namespace Pacco.Services.Deliveries.Infrastructure.Mongo.Documents;
+
+public class DeliveryDocument : IIdentifiable<Guid>
 {
-    public class DeliveryDocument : IIdentifiable<Guid>
-    {
-        public Guid Id { get; set; }
-        public Guid OrderId { get; set; }
-        public DeliveryStatus Status { get; set; }
-        public string Notes { get; set; }
-        public IEnumerable<DeliveryRegistrationDocument> Registrations { get; set; }
-    }
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public DeliveryStatus Status { get; set; }
+    public string Notes { get; set; }
+    public IEnumerable<DeliveryRegistrationDocument> Registrations { get; set; }
 }
--- src/Pacco.Services.Deliveries.Infrastructure/Services/DateTimeProvider.cs
+++ src/Pacco.Services.Deliveries.Infrastructure/Services/DateTimeProvider.cs
@@ -1,10 +1,10 @@
 using System;
+
 using Pacco.Services.Deliveries.Application.Services;
 
-namespace Pacco.Services.Deliveries.Infrastructure.Services
+namespace Pacco.Services.Deliveries.Infrastructure.Services;
+
+public class DateTimeProvider : IDateTimeProvider
 {
-    public class DateTimeProvider : IDateTimeProvider
-    {
-        public DateTime Now  => DateTime.UtcNow;
-    }
+    public DateTime Now => DateTime.UtcNow;
 }
