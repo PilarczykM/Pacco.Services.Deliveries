aed625a8b5b02e3d35835660d70741e2b28ef83f
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-11-09 06:29:39 +0100

    Project init
    Old version 3.1
    Lack of style

---
+++ src/Pacco.Services.Deliveries.Core/Entities/AggregateId.cs
@@ -0,0 +1,50 @@
+using System;
+
+using Pacco.Services.Deliveries.Core.Exceptions;
+
+namespace Pacco.Services.Deliveries.Core.Entities;
+
+public class AggregateId : IEquatable<AggregateId>
+{
+    public Guid Value { get; }
+
+    public AggregateId()
+    {
+        Value = Guid.NewGuid();
+    }
+
+    public AggregateId(Guid value)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new InvalidAggregateIdException();
+        }
+
+        Value = value;
+    }
+
+    public bool Equals(AggregateId other)
+    {
+        if (other is null) return false;
+        return ReferenceEquals(this, other) || Value.Equals(other.Value);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((AggregateId)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    public static implicit operator Guid(AggregateId id)
+        => id.Value;
+
+    public static implicit operator AggregateId(Guid id) => new(id);
+
+    public override string ToString() => Value.ToString();
+}
